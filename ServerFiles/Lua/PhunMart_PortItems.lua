ef7ea87386df2451c17a0a710e3c4b7bed9aade6
PhunZoider <projectphunzoid@gmail.com>
2024-06-16 19:30:32 +0100

    preview resize

--- ServerFiles/Lua/PhunMart_PortItems.lua
+++ ServerFiles/Lua/PhunMart_PortItems.lua
@@ -1,81 +1,92 @@
-return {
-	{
-		display = {
-			label = "Louisville",
-			type = "PORT",
-		},
-		receive = {{
-			type = "PORT",
-			value = {
-				{x=12988, y=1303, z=2},
-				{x=12962, y=1368, z=0}
-			}
-		}},
-	},
-	{
-		display = {
-			label = "Muldraugh",
-			type = "PORT",
-		},
-		inventory=false,
-		receive = {{
-			type = "port",
-			value = {
-				{x=10597, y=10334, z=0},
-				{x=10597, y=10338, z=0}
-			}
-		}},
-	},
-	{
-		display = {
-			label = "Rosewood",
-			type = "PORT",
-		},
-		inventory=false,
-		receive = {{
-			type = "port",
-			value = {
-				{x=8073, y=11666, z=1},
-			}
-		}},
-	},
-	{
-		display = {
-			label = "Riverside",
-			type = "PORT",
-		},
-		inventory=false,
-		receive = {{
-			type = "port",
-			value = {
-				{x=6430, y=5433, z=0},
-			}
-		}},
-	},
-	{
-		display = {
-			label = "West Point",
-			type = "PORT",
-		},
-		inventory=false,
-		receive = {{
-			type = "port",
-			value = {
-				{x=11373, y=6792, z=0},
-			}
-		}},
-	},
-	{
-		display = {
-			label = "March Ridge",
-			type = "PORT",
-		},
-		inventory=false,
-		receive = {{
-			type = "port",
-			value = {
-				{x=10044, y=12735, z=0},
-			}
-		}},
-	},	
-}
+return {{
+    display = {
+        label = "Louisville",
+        type = "PORT"
+    },
+    receive = {{
+        type = "PORT",
+        value = {{
+            x = 12988,
+            y = 1303,
+            z = 2
+        }, {
+            x = 12962,
+            y = 1368,
+            z = 0
+        }}
+    }}
+}, {
+    display = {
+        label = "Muldraugh",
+        type = "PORT"
+    },
+    inventory = false,
+    receive = {{
+        type = "port",
+        value = {{
+            x = 10597,
+            y = 10334,
+            z = 0
+        }, {
+            x = 10597,
+            y = 10338,
+            z = 0
+        }}
+    }}
+}, {
+    display = {
+        label = "Rosewood",
+        type = "PORT"
+    },
+    inventory = false,
+    receive = {{
+        type = "port",
+        value = {{
+            x = 8073,
+            y = 11666,
+            z = 1
+        }}
+    }}
+}, {
+    display = {
+        label = "Riverside",
+        type = "PORT"
+    },
+    inventory = false,
+    receive = {{
+        type = "port",
+        value = {{
+            x = 6430,
+            y = 5433,
+            z = 0
+        }}
+    }}
+}, {
+    display = {
+        label = "West Point",
+        type = "PORT"
+    },
+    inventory = false,
+    receive = {{
+        type = "port",
+        value = {{
+            x = 11373,
+            y = 6792,
+            z = 0
+        }}
+    }}
+}, {
+    display = {
+        label = "March Ridge",
+        type = "PORT"
+    },
+    inventory = false,
+    receive = {{
+        type = "port",
+        value = {{
+            x = 10044,
+            y = 12735,
+            z = 0
+        }}
+    }}
+}}
